6f91a1089c07baf02f34ad3c907ebfaac4a95d90
davidhickey <davidhickey423@gmail.com>
2019-01-16 15:33:22 -0500

    successful api call to webster's dict

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider, Form, Input} from 'semantic-ui-react'
+// import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
@@ -22,32 +23,25 @@ class App extends Component {
   handleSubmit(event) {
    event.preventDefault();
    this.setState({ card_value: this.state.card_input });
-   // console.log(this.state.card_value);
+   let word = this.state.card_input;
+   let url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'+ word +'?key=c7ab16a7-6f5a-45f3-b42d-d60b0fab9d91'
+     fetch(url,{
+         method: "GET"
+       }).then(response => {
+         console.log("response", response);
+         response.json().then(data =>{
+           console.log(data[0].shortdef);
+           this.setState({
+            card_definition: data[0].shortdef[0]
+             })
+         })
+     })
   }
   updateInput(event) {
      this.setState({card_input: event.target.value});
   }
-  componentDidUpdate(prevState){
-      let value = this.state.card_value;
-      console.log(value);
-
-     //  let url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'+ value +'?key=c7ab16a7-6f5a-45f3-b42d-d60b0fab9d91'
-     //
-     // fetch(url,{
-     //      method: "GET"
-     //    }).then(response => {
-     //      console.log("response", response);
-     //
-     //      response.json().then(data =>{
-     //        console.log(data[0].shortdef);
-     //        this.setState({
-     //          card_definition: data[0].shortdef[0]
-     //        });
-     //      });
-     //    })
 
 
-  }
 
 
   render() {
@@ -68,7 +62,7 @@ class App extends Component {
         </form>
         <div className="segment results">
           <p>{this.state.card_value}</p>
-          <p>{this.state_card_definition}</p>
+          <p>{this.state.card_definition}</p>
 
         </div>
       </div>
