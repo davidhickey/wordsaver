d380e8a3aab2845eaef6534baaaa0955bc10ad96
davidhickey <davidhickey423@gmail.com>
2019-02-01 12:14:01 -0500

    sets up def and word quiz to show

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -16,7 +16,6 @@ quizStart(type, start, questions){
 //grabs cards from props for quiz
 grabCards(type, questions){
   var cards = this.props.quizData.questions
-    // console.log(cards)
   var cardData = Object.keys(cards).map((card, i)=>{
       return [i, card, cards[card][0], cards[card].slice(1, 4)]
   })
@@ -25,7 +24,6 @@ grabCards(type, questions){
 
 // //Works to shuffle order of cards and organize front and back of card
 shuffleQuiz(array){
-  // var array = cardData
   var currentIndex = array.length, temporaryValue, randomIndex;
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
@@ -40,25 +38,76 @@ shuffleQuiz(array){
   return array
 }
 
-
-
-
-
+// allTheCards(){
+//   let startQuiz = this.props.quizData.quiz_started
+//   let type = this.props.quizData.quiz_type
+//   if(startQuiz){
+//     console.log('quiz started!')
+//   let questions = this.props.quizData.questions;
+//   const shuffledQuestions = this.grabCards(type, questions)
+//   const showShuffledCards = Object.keys(shuffledQuestions).map((key) => {
+//     console.log('in shuffled card const');
+//     return <p>{'Hi there'}</p>
+// })
+//
+//     if(type === 'Def'){
+//       console.log('Def Quiz!')
+//       // return()
+//
+//     }
+//     if(type === 'Word')
+//     console.log('Word Quiz!')
+//
+//
+// }
+//
+// }
 
   render() {
-    let startQuiz = this.props.quizData.quiz_started
-    let type = this.props.quizData.quiz_type
-    if(startQuiz){
-      console.log('quiz started!')
-    let questions = this.props.quizData.questions;
-    let shuffledQuestions = this.grabCards(type, questions)
-    console.log(shuffledQuestions)
+    var startQuiz = this.props.quizData.quiz_started
+    var type = this.props.quizData.quiz_type
+    var questions = this.props.quizData.questions
+if(startQuiz){
+    if(type === 'Def'){
+      var def = true
+       }
+       if(type === 'Word'){
+         var word = true
+
+   }
+ }
 
 
-  }
-//     console.log(questions)
-//     let shuffledQuestions = this.shuffleQuiz([questions])
-// console.log(shuffledQuestions)
+
+    //  function ShowShuffledCards() {
+    //     // var startQuiz = this.props.quizData.quiz_started
+    //     // var type = this.props.quizData.quiz_type
+    //     // if(this.props.quizData.quiz_started === 'true'){
+    //       console.log('quiz started!')
+    //       var type = this.props.quizData.quiz_type
+    //
+    //     let questions = this.props.quizData.questions;
+    //
+    //     const test = <p>{'test'}</p>;
+    //     const shuffledQuestions = this.grabCards(questions)
+    //     const showShuffledCards = Object.keys(shuffledQuestions).map((key) => {
+    //       return (<p>{key}</p>)
+    //   })
+
+      //     if(type === 'Def'){
+      //       console.log('Def Quiz!')
+      //       // return()
+      //
+      //     }
+      //     if(type === 'Word'){
+      //     console.log('Word Quiz!')
+      //
+      //
+      // }
+    // }
+  // }
+
+
 
 
     return (
@@ -68,6 +117,18 @@ shuffleQuiz(array){
           <Button onClick={() => this.quizStart('Def', true)}>Definition Quiz</Button>
           <Button onClick={() => this.quizStart('Word', true)}>Word Quiz</Button>
           </div>
+          {startQuiz ? (
+            <p>{'Quiz Started'}</p>
+          ) : (null)
+        }
+            {def ? (
+              <p>{'Def quiz fo show'}</p>)
+              : (null)
+            }
+            {word ? (
+              <p>{'Word quiz fo show'}</p>)
+              : (null)
+            }
       </Segment>
     )
   }
